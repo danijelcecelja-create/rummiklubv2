bf5d7452bb7c8105e49649550a6f3f2345a1012f
danijelcecelja-create <danijelcecelja@gmail.com>
2026-09-20 14:50:40 +0200

    Fix HTML closing tag for player name cell

--- rummiklub.js
+++ rummiklub.js
@@ -228,7 +228,7 @@ function render()
                     <td class="playerName">
     <span class="rankingIndex">${index + 1}</span>
     ${speler.naam}
-</t        
+</td>       
                     <td><b>${speler.score}</b></td>
                     <td>${speler.spellen}</td>
                     <td>${speler.wins}</td>
